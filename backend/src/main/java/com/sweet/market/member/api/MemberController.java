06da258a40e2aa2040f8a790715d5a3bc34bbac5
sweet.dh <lucky00@daou.co.kr>
2026-06-08 17:08:37 +0900

    feat: add current member api

---
+++ backend/src/main/java/com/sweet/market/member/api/MemberController.java
@@ -0,0 +1,27 @@
+package com.sweet.market.member.api;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sweet.market.auth.security.AuthenticatedMember;
+import com.sweet.market.common.api.ApiResponse;
+import com.sweet.market.member.application.MemberService;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/me")
+    public ApiResponse<MemberMeResponse> me(Authentication authentication) {
+        AuthenticatedMember authenticatedMember = (AuthenticatedMember) authentication.getPrincipal();
+        return ApiResponse.ok(memberService.findMe(authenticatedMember.id()));
+    }
+}
